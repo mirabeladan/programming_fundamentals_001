94c21730e0b0df1075a9af389e35ef3c9ca98bd3
Mirabela Dan <mirabeladan@live.de>
2019-11-05 15:40:57 +0000

    added answers

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,32 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+function makeHalfPrice (price) {
+    return price *0.5;
+}
+
+function countBooks (array) {
+    return array.length;
+}
+
+function isInStock(book) {
+    return book.quantity > 0
+}
+
+function getTotalOrderPrice(price, quantity) {
+    return (price*quantity) * 1.2
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
+
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,60 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+
 });
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns the price with a 50% discount applied", () => {
+    expect(utils.makeHalfPrice(88)).toBe(44);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns the total number of books in the array", () => {
+   expect(utils.countBooks(["Harry Potter", "Norwegian Wood", "Malta Travelguide"])).toBe(3);
+  });
+});
+
+describe("utils.isInStock", () => {
+  test("returns true if the title is in stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+  
+    expect(utils.isInStock(book)).toBe(true);
+  });
+})
+
+describe("utils.isInStock", () => {
+  test("returns false if the title is not in stock", () => {
+    const book = {
+      title: "The Diaries",
+      author: "Carol",
+      yearOfPublication: 1992,
+      quantity: 0
+    };
+  
+    expect(utils.isInStock(book)).toBe(false);
+  });
+})
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns price multiplied by quantity plus VAT", () => {
+    expect(utils.getTotalOrderPrice(100, 2)).toBe(240);
+  });
+});
+
